565f104ded818d39ea4ed3739fdea363ddd206ba
sweet666 <safronenkoandrey@gmail.com>
2017-05-13 19:49:59 +0300

    fixed validation bug with registration

--- src/main/java/by/safronenko/config/WebConfiguration.java
+++ src/main/java/by/safronenko/config/WebConfiguration.java
@@ -1,6 +1,9 @@
 package by.safronenko.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,5 +13,13 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/views/", ".jspx");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
