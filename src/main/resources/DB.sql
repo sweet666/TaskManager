0ee7540891d92662ad60f5adeccf8fb7a3ac3645
sweet666 <safronenkoandrey@gmail.com>
2017-05-16 03:43:02 +0300

    Localization messages for tasks

--- src/main/resources/DB.sql
+++ src/main/resources/DB.sql
@@ -15,24 +15,86 @@ CREATE DATABASE IF NOT EXISTS `taskmanager` /*!40100 DEFAULT CHARACTER SET utf8 
 USE `taskmanager`;
 
 
+-- Дамп структуры для таблица taskmanager.hibernate_sequence
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы taskmanager.hibernate_sequence: ~2 rows (приблизительно)
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+	(7),
+	(7);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица taskmanager.roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы taskmanager.roles: ~2 rows (приблизительно)
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `name`) VALUES
+	(1, 'ROLE_USER'),
+	(2, 'ROLE_ADMIN');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+
 -- Дамп структуры для таблица taskmanager.task
 CREATE TABLE IF NOT EXISTS `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
   `is_end` smallint(5) DEFAULT '0',
   `start_date` varchar(10) DEFAULT NULL,
   `expire_date` varchar(255) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы taskmanager.task: ~10 rows (приблизительно)
+-- Дамп данных таблицы taskmanager.task: ~0 rows (приблизительно)
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` (`id`, `name`, `description`, `is_end`, `start_date`, `expire_date`) VALUES
-	(1, 'test1', 'descr1', 0, '24-05-2017', '25-25-2017'),
-	(2, 'test2', 'descr2', 0, '24-05-2017', '25-05-2017'),
-	(3, 'test3', 'descr3', 1, '24-05-2017', '25-05-2017');
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица taskmanager.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы taskmanager.users: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+	(4, 'sweet666', '$2a$10$mRyCozkaBbvetSxCr2ZmTeGmdKucbtzZBexr0mAV3RuNvWnst8IAi'),
+	(5, 'test666', '$2a$10$zQIfPDwkAw.RF8xg6CN6au4hlA8eR99k3aZodEfLGf5GvP2f5CTB.');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица taskmanager.user_roles
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`,`role_id`),
+  KEY `FKh8ciramu9cc9q3qcqiv4ue8a6` (`role_id`),
+  CONSTRAINT `FKh8ciramu9cc9q3qcqiv4ue8a6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы taskmanager.user_roles: ~6 rows (приблизительно)
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
+	(4, 1),
+	(5, 1),
+	(4, 2),
+	(5, 2);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
