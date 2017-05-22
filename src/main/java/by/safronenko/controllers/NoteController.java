df61a3cb26086905c8ac348ce5986035d359d140
sweet666 <safronenkoandrey@gmail.com>
2017-05-22 13:20:53 +0300

    NoteController update

--- src/main/java/by/safronenko/controllers/NoteController.java
+++ src/main/java/by/safronenko/controllers/NoteController.java
@@ -5,7 +5,10 @@ import by.safronenko.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -23,4 +26,22 @@ public class NoteController {
 
         return "notes";
     }
+
+    @RequestMapping(value = "/addnote", method = RequestMethod.POST)
+    public String addTask(@ModelAttribute("note") Note note) {
+
+        noteService.addNote(note);
+
+        return "redirect:/notes";
+    }
+
+    @RequestMapping(value = "/notes/delete/{taskId}")
+    public String deleteNote(@PathVariable("taskId") int noteId) {
+
+        noteService.deleteNote(noteId);
+
+        return "redirect:/tasks";
+    }
+
+
 }
